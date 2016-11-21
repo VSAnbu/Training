e4a63a35e1a30ab7549e62d4d5ebbe4407d8a6e4
VSAnbu <vs.anbu@gmail.com>
2016-11-21 18:10:51 +0530

    Create AppTest.java

---
+++ src/test/java/com/javacodegeeks/AppTest.java
@@ -0,0 +1,17 @@
+package com.javacodegeeks;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+	
+	@Test
+	public void testApp() {
+		App appObject = new App();
+		Assert.assertEquals(appObject.reverseString("Test!"), "!tseT");
+	}
+}
+
